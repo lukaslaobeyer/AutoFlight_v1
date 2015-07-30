7c0e05495e01e9511c6f5df43bb2aa51b9a496af
Lukas Lao Beyer <lukaslaobeyer@gmail.com>
2015-07-30 17:40:11 +0200

    Add Debug/Release configuration

--- src/afconstants.h
+++ src/afconstants.h
@@ -6,7 +6,7 @@
 namespace autoflight
 {
 	const std::string SOFTWARE_VERSION = "Beta 1.0";
-	const std::string BUILD_NUMBER     = "4070927";
+	const std::string BUILD_NUMBER     = "ff7b2aa";
 }
 
 namespace error
